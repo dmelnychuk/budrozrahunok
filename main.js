bb053d87281a0927fbfb16cebcdbe2819c177735
live7374 <5358207@gmail.com>
2023-06-19 00:02:05 +0300

    updated figure rendering with inputed value

--- main.js
+++ main.js
@@ -1,26 +1,24 @@
 import {
-    Camera,
-    Material,
-    Texture,
-    BoxGeometry,
-    Mesh,
-    MeshBasicMaterial,
-    Scene,
-    WebGLRenderer,
-    PerspectiveCamera,
-    BoxBufferGeometry,
-    Color
+  Camera,
+  Material,
+  Texture,
+  BoxGeometry,
+  Mesh,
+  MeshBasicMaterial,
+  Scene,
+  WebGLRenderer,
+  PerspectiveCamera,
+  BoxBufferGeometry,
+  Color
+} from "https://cdn.skypack.dev/three@0.132.2";
 
-  } from "https://cdn.skypack.dev/three@0.132.2";
-  
-  import { OrbitControls } from "https://cdn.skypack.dev/three@0.132.2/examples/jsm/controls/OrbitControls.js";
+import { OrbitControls } from "https://cdn.skypack.dev/three@0.132.2/examples/jsm/controls/OrbitControls.js";
 
-  import { getFoundationSize } from './foundation.js';
-
+import { getFoundationSize } from './foundation.js';
 
 // Initialize Three.js
 const scene = new Scene();
-scene.background = new Color ('whitesmoke');
+scene.background = new Color('whitesmoke');
 
 const camera = new PerspectiveCamera(75, 1, 0.1, 1000); // Use an initial aspect ratio of 1
 const renderer = new WebGLRenderer();
@@ -39,7 +37,7 @@ function updateRendererSize() {
 updateRendererSize();
 
 // Add objects, lights, etc. to the scene
-const geometry = new BoxBufferGeometry(1, 3, 4, 9, 9, 9);
+const geometry = new BoxBufferGeometry(1, 1, 1, 9, 9, 9);
 const material = new MeshBasicMaterial({ color: 0x00ff00, wireframe: true });
 const cube = new Mesh(geometry, material);
 scene.add(cube);
@@ -47,15 +45,35 @@ scene.add(cube);
 // Position the camera
 camera.position.z = 5;
 
+// Animation variables
+let animationId = null;
+
 // Render the scene
-function animate() {
-  requestAnimationFrame(animate);
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+function renderScene() {
+  const foundationSize = getFoundationSize();
+  const width = foundationSize.fdwidth;
+  const length = foundationSize.fdlength;
+  const depth = foundationSize.fddepth;
+
+  cube.scale.set(width, length, depth);
   renderer.render(scene, camera);
 }
 
-animate();
+// Animation function
+function animate() {
+  animationId = requestAnimationFrame(animate);
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+  renderScene();
+}
 
 // Update renderer size when window is resized
 window.addEventListener('resize', updateRendererSize);
+
+// Trigger the scene rendering and animation when the "Calculate" button is clicked
+window.addEventListener('click', () => {
+  renderScene();
+  if (!animationId) {
+    animate();
+  }
+});
